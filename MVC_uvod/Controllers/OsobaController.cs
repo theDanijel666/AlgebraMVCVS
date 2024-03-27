d12605d0596fa3189b9dcb018e24c76b49e93e4a
Danijel Pobi <121439317+theDanijel666@users.noreply.github.com>
2024-03-27 21:58:42 +0100

    Add project files.

---
+++ MVC_uvod/Controllers/OsobaController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_uvod.Models;
+
+namespace MVC_uvod.Controllers
+{
+    public class OsobaController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(Osoba o)
+        {
+            ViewBag.Ime = "Moje ime :)";
+            //Osoba o = new Osoba()
+            //{
+            //    Ime = "Nikola",
+            //    Prezime = "Tesla",
+            //    DatumRodjenja = new DateTime(1867, 6, 21)
+            //};
+            return View(o);
+        }
+
+        [HttpPost]
+        public IActionResult OsobaPost()
+        {
+            var formString = Request.Form;
+
+            Osoba nova_osoba= new Osoba();
+            nova_osoba.Ime = formString["Ime"];
+            nova_osoba.Prezime = formString["Prezime"];
+            nova_osoba.DatumRodjenja = DateTime.Parse(formString["DatumRodjenja"]);
+
+            return View("Index", nova_osoba);
+        }
+    }
+}
---
+++ MVC_uvod/Models/Osoba.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC_uvod.Models
+{
+    public class Osoba
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public DateTime DatumRodjenja { get; set; }
+    }
+}
